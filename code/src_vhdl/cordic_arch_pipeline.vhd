5c89f0bedbc68f0ada507a80352e5a34030ff59a
André <andre_miguel_costa@hotmail.com>
2025-05-07 14:17:27 +0200

    fix(pipeline): correctly map signals

--- code/src_vhdl/cordic_arch_pipeline.vhd
+++ code/src_vhdl/cordic_arch_pipeline.vhd
@@ -61,43 +61,43 @@ architecture pipeline of cordic is
              );
     end component;
 
-    type iter_values_array_t is array (0 to 11) of std_logic_vector(DATASIZE - 1 downto 0);
-    type iter_phi_array_t is array (0 to 11) of std_logic_vector(PHI_OUTPUTSIZE - 1 downto 0);
-    type iter_data_valid_array_t is array (0 to 11) of std_logic;
+    type iter_values_array_t is array (0 to 10) of std_logic_vector(DATASIZE - 1 downto 0);
+    type iter_phi_array_t is array (0 to 10) of std_logic_vector(PHI_OUTPUTSIZE - 1 downto 0);
+    type original_quadrant_array_t is array (0 to 10) of std_logic_vector(1 downto 0);
 
+    signal original_quadrant_id_s   : original_quadrant_array_t;
     signal re_i_s                   : iter_values_array_t; 
     signal im_i_s                   : iter_values_array_t; 
-    signal phi_i_s                  : iter_phi_array_t; 
     signal re_o_s                   : iter_values_array_t; 
     signal im_o_s                   : iter_values_array_t; 
+    signal phi_i_s                  : iter_phi_array_t; 
     signal phi_o_s                  : iter_phi_array_t; 
 
-    signal data_valid_s             : std_logic_vector(11 downto 0);
-    signal stop_s                   : std_logic;
-    signal original_quadrant_id_s   : std_logic_vector(1 downto 0);
-    signal signals_exchanged_s      : std_logic;
+    signal amp_s                    : std_logic_vector(amp_o'range);
+    signal phi_s                    : std_logic_vector(phi_o'range);
+    signal data_valid_s             : std_logic_vector(10 downto 0);
+    signal signals_exchanged_s      : std_logic_vector(10 downto 0);
 
-    signal amp_s       :  std_logic_vector(amp_o'range);
-    signal phi_s       :  std_logic_vector(phi_o'range);
-    signal ready_s : std_logic;
+    signal stop_s                   : std_logic;
+    signal ready_s                  : std_logic;
 
 begin
     pre_treatment: entity work.cordic_pre_treatment
     port map(
-       re_i => re_i_s(0),
-       im_i => im_i_s(0),
+       re_i => re_i,
+       im_i => im_i,
        re_o => re_o_s(0),
-       im_o => re_o_s(0),
-       original_quadrant_id_o => original_quadrant_id_s,
-       signals_exchanged_o => signals_exchanged_s
+       im_o => im_o_s(0),
+       original_quadrant_id_o => original_quadrant_id_s(0),
+       signals_exchanged_o => signals_exchanged_s(0)
     );
 
     process(clk_i)
     begin
         if rising_edge(clk_i) then
             if stop_s = '0' then
-                re_i_s(1) <= re_o_s(0);
-                im_i_s(1) <= re_o_s(0);
+                re_i_s(0) <= re_o_s(0);
+                im_i_s(0) <= im_o_s(0);
                 phi_i_s(0) <= (others => '0');
                 data_valid_s(0) <= ready_s and valid_i;
             end if;
@@ -106,22 +106,24 @@ begin
     
     cordic_iterations: for i in 1 to 10 generate
         iteration: cordic_iteration port map (
-            re_i  => re_i_s(i),   
-            im_i  => im_i_s(i), 
+            re_i  => re_i_s(i - 1),   
+            im_i  => im_i_s(i - 1), 
             phi_i => phi_i_s(i - 1),
-            re_o  => re_o_s(i + 1),    
-            im_o  => im_o_s(i + 1),
-            phi_o => phi_o_s(i),
-            iter_i => std_logic_vector(to_unsigned(i + 1, 4))
+            re_o  => re_o_s(i),    
+            im_o  => im_o_s(i),
+            phi_o => phi_o_s(i - 1),
+            iter_i => std_logic_vector(to_unsigned(i, 4))
         );
         process(clk_i)
         begin
             if rising_edge(clk_i) then
                 if stop_s = '0' then
-                    re_i_s(i + 1) <= re_o_s(i + 1);
-                    re_i_s(i + 1) <= im_o_s(i + 1);
+                    re_i_s(i) <= re_o_s(i);
+                    im_i_s(i) <= im_o_s(i);
                     phi_i_s(i) <= phi_o_s(i - 1);
                     data_valid_s(i) <= data_valid_s(i - 1);
+                    original_quadrant_id_s(i) <= original_quadrant_id_s(i - 1);
+                    signals_exchanged_s(i) <= signals_exchanged_s(i - 1);
                 end if;
             end if;
         end process;
@@ -129,11 +131,11 @@ begin
 
     post_treatment: entity work.cordic_post_treatment
     port map(
-       re_i => re_i_s(11),
-       im_i => im_i_s(11),
-       original_quadrant_id_i => original_quadrant_id_s,
-       signals_exchanged_i => signals_exchanged_s,
-       phi_i => phi_i_s(11),
+       re_i => re_i_s(10),
+       im_i => im_i_s(10),
+       original_quadrant_id_i => original_quadrant_id_s(10),
+       signals_exchanged_i => signals_exchanged_s(10),
+       phi_i => phi_i_s(10),
        amp_o => amp_s,
        phi_o => phi_s
     );
